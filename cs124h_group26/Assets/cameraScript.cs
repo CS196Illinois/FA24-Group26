5f441ac06777378b7033ba6a465427eb5496bdce
hnanna2 <hnanna2@illinois.edu>
2024-11-26 16:56:21 -0600

    Toggle Visibility, adjusted movement
    The camera will now follow for any number of toggles, and toggle visibility will turn off after clicked

--- cs124h_group26/Assets/cameraScript.cs
+++ cs124h_group26/Assets/cameraScript.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 
 public class cameraScript : MonoBehaviour
 {
-    public Toggle posToggle;
+    /*public Toggle posToggle;
     public Vector3 newPos;
     public float watTime = 2f;
     public float moveDuration = 1f;
@@ -32,8 +32,45 @@ public class cameraScript : MonoBehaviour
             yield return null; 
         }
         transform.position = newPos;
+    }*/
+
+    public Toggle[] checkboxes;
+    public float moveSpeed = 7f;
+    private Vector3[] targetPositions;
+    int xPos = 300; 
+
+    void Start() {
+        targetPositions = new Vector3[checkboxes.Length];
+        for(int i = 0; i < checkboxes.Length; i++) {
+            if (i % 2 == 0) {
+                targetPositions[i] = new Vector3(xPos + 200, 0, 500);
+                int index = i;
+                checkboxes[i].onValueChanged.AddListener(delegate { OnCheckboxChanged(index); });
+            } else {
+                targetPositions[i] = new Vector3(xPos - 400, 50, 500);
+                int index = i;
+                checkboxes[i].onValueChanged.AddListener(delegate { OnCheckboxChanged(index); });
+            }
+        }
     }
 
+    void OnCheckboxChanged(int index) {
+        if (checkboxes[index].isOn) {
+            checkboxes[index].gameObject.SetActive(false);
+            StartCoroutine(MoveCameraToTarget(targetPositions[index]));
+        }
+    }
+    IEnumerator MoveCameraToTarget(Vector3 target) {
+        Vector3 startPos = transform.position;
+        float timeElapsed = 0;
+
+        while (timeElapsed < 1f) {
+            transform.position = Vector3.Lerp(startPos, target, timeElapsed);
+            timeElapsed += Time.deltaTime * moveSpeed;
+            yield return null;
+        }
+        transform.position = target;
+    }
     // Update is called once per frame
     void Update()
     {
